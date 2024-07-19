32ad0646a2913539b58cb7a5614aadb110ca4201
MJ2457 <129284258+MJ2457@users.noreply.github.com>
2024-07-19 22:06:45 +0100

    git commit "committing all files to git"

---
+++ src/components/Workout.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
+
+import PropTypes from 'prop-types';
+
+export default function Workout(props) {
+    const { workout } = props
+    return (
+        <SectionWrapper id={'workout'} header={"welcome to"} title={['The', 'DANGER', 'zone']}>
+            <div className='flex flex-col gap-4'>
+                {workout.map((exercise, i) => {
+                    return (
+                        <ExerciseCard i={i} exercise={exercise} key={i} />
+                    )
+                })}
+            </div>
+        </SectionWrapper>
+    )
+}
+
+Workout.propTypes = {
+    workout: PropTypes.array.isRequired
+};
+
+
+
